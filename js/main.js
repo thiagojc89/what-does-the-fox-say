6d733146a1d8a6d867c6714256a547929455e9a8
thiago <thiago.cavalcante@generalassemb.ly>
2021-07-01 16:19:15 -0400

    audio path fixed

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const notFoxImg = document.querySelector("#notFox")
 
 notFoxImg.addEventListener("click", evt => {
     if (evt.target.id !== "notFox") {
-        const audioElement = new Audio(`../audio/${evt.target.id}.mp3`)
+        const audioElement = new Audio(`audio/${evt.target.id}.mp3`)
         audioElement.volume = .01
         audioElement.play()
     }
